1af455999b37118ceb9d78a321e3d8d8ffd24a60
Thiwanka Somachandra <thivankas@outlook.com>
2025-01-16 06:23:31 +0200

    Solution using bitwise operators

--- Level 2/reverse_bits/reverse_bits.c
+++ Level 2/reverse_bits/reverse_bits.c
@@ -1,13 +1,13 @@
 unsigned char	reverse_bits(unsigned char octet)
 {
-	int		i = 8;
-	unsigned char	res = 0;
-
-	while (i > 0)
+	unsigned char res = 0x0;
+	int i = 0;
+	while(i < 8)
 	{
-		res = res * 2 + (octet % 2);
-		octet = octet / 2;
-		i--;
+		res = res << 1;
+		res = res | (octet & 1);
+		octet = octet >> 1;
+		i++;
 	}
 	return (res);
 }
