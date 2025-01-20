6906167b70f97be60d62337568abfbb347e023ac
Thiwanka Somachandra <thivankas@outlook.com>
2025-01-20 04:06:13 +0200

    solution using HCF

--- Level 3/lcm/lcm.c
+++ Level 3/lcm/lcm.c
@@ -1,17 +1,18 @@
-unsigned int lcm(unsigned int a, unsigned int b)
+unsigned int	hcf(unsigned int a, unsigned int b)
 {
-	unsigned int n;	
-
-	if (a == 0 || b == 0)
-		return (0);
-	if (a > b)
-		n = a;
-	else
-		n = b;
-	while (1)
+	unsigned int temp;
+	while (b != 0)
 	{
-		if (n % a == 0 && n % b == 0)
-			return (n);
-		++n;
+		temp = b;
+		b = a % b;
+		a = temp;
 	}
+	return (a);
+}
+
+unsigned int	lcm(unsigned int a, unsigned int b)
+{
+	if(a == 0 || b == 0)
+		return (0);
+	return ((a * b)/ hcf(a,b));
 }
